f110a782b566d2510f7209e93708b997f26e9107
margaritakop <margarita.kopniczky@gmail.com>
2018-10-26 14:45:41 +0100

    basic class constructor

--- models/person.js
+++ models/person.js
@@ -1,1 +1,9 @@
-// Your Person class goes here!
+class Person {
+    constructor(firstName, surname, dob){
+        this.firstName = firstName
+        this.surname = surname
+        this.dob = dob
+    }
+}
+
+module.exports = Person
--- test/person.test.js
+++ test/person.test.js
@@ -1,11 +1,13 @@
 const chai = require('chai')
 const expect = chai.expect
 
-describe("Person", ()=> {
+const Person = require('../models/person.js')
+
+describe('Person', ()=> {
     it ('should initialise properly', () => {
-        var person = new person('Joe', 'Bloggs', '1 Jan 1990')
-        experct(person.firstname).to.equal('Joe')
-        experct(person.surname).to.equal('Bloggs')
-        experct(person.dob).to.equal('1 Jan 1990')
+        var person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+        expect(person.firstName).to.equal('Joe')
+        expect(person.surname).to.equal('Bloggs')
+        expect(person.dob).to.equal('1 Jan 1990')
     })
 })
