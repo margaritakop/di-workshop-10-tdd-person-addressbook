f595498cc5a6f53a0bba43d0543ceb0cfe2cb826
margaritakop <margarita.kopniczky@gmail.com>
2018-10-26 14:15:07 +0100

    test for the Person object

--- test/person.test.js
+++ test/person.test.js
@@ -1,4 +1,11 @@
 const chai = require('chai')
 const expect = chai.expect
 
-// Your Person test goes here!
+describe("Person", ()=> {
+    it ('should initialise properly', () => {
+        var person = new person('Joe', 'Bloggs', '1 Jan 1990')
+        experct(person.firstname).to.equal('Joe')
+        experct(person.surname).to.equal('Bloggs')
+        experct(person.dob).to.equal('1 Jan 1990')
+    })
+})
