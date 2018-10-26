6ccf8ce86c3e51b041a99522cd78e2401d8cc07c
margaritakop <margarita.kopniczky@gmail.com>
2018-10-26 14:52:11 +0100

    function to capitalise first letter in the class

--- models/person.js
+++ models/person.js
@@ -1,6 +1,6 @@
 class Person {
     constructor(firstName, surname, dob){
-        this.firstName = firstName
+        this.firstName = firstName.charAt(0).toUpperCase() + firstName.slice(1)
         this.surname = surname
         this.dob = dob
     }
