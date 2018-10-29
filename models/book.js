cb2db84eb676659695e768de9b82516351a10963
margaritakop <margarita.kopniczky@gmail.com>
2018-10-29 11:31:02 +0000

    return the first occurance of firstName search

--- models/book.js
+++ models/book.js
@@ -5,6 +5,14 @@ class Book {
     add(person){
         this.entries.push(person)
     }
+
+    findByFirstName(firstNameSearched){
+        for (var i in (this.entries)){
+            if (this.entries[i].firstName == firstNameSearched ){  //there should be a nicer way to iterate
+                return this.entries[i]
+            }
+        }
+        }
 }
 
 module.exports = Book
