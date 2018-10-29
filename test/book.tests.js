07c46ad88c599a0b6a7ce3315c9a051c3e8667a2
margaritakop <margarita.kopniczky@gmail.com>
2018-10-29 10:53:34 +0000

    add add function to book

--- test/book.tests.js
+++ test/book.tests.js
@@ -2,10 +2,18 @@ const chai = require('chai')
 const expect = chai.expect
 
 const Book = require('../models/book.js')
+const Person = require('../models/person.js')
 
 describe('Book', ()=> {
     it ('should initialise properly', () => {
         var book = new Book
         expect(book.entries).to.deep.equal([])
     })
+
+    it('should add person to the array of entries', () =>{
+        var person = new Person('Joe', 'Bloggs', '1 Jan 1990')
+        var book = new Book
+        expect(book.add(person)).to.deep.equal([person])
+    }
+    )
 })
